f6d27860f1daada640128c71b8efcde8f621dd6c
Nikita Popov <popov.n294@gmail.com>
2021-11-19 19:20:36 +0300

    First elements took in count

--- exponential_moving_average.py
+++ exponential_moving_average.py
@@ -15,20 +15,23 @@ def exponential_moving_avereage(data, smooth_interval = 2):
         sum += data[step]
     previous_ma_value = sum/smooth_interval
 
-    for step in range(smooth_interval, len(data)):
-        filtered_values[step] = previous_ma_value
+    for step in range(smooth_interval-1):
         previous_ma_value = alpha*data[step]+(1-alpha)*previous_ma_value
-        # print("Data: ", data[step], "Prev value: ", previous_ma_value)
+        filtered_values[step] = previous_ma_value
+
+    sum = 0
+
+    for step in range(smooth_interval):
+        sum += data[step]
+    previous_ma_value = sum/smooth_interval
+    filtered_values[smooth_interval-1] = previous_ma_value
+
+    for step in range(smooth_interval, len(data)):
+        previous_ma_value = alpha*data[step]+(1-alpha)*previous_ma_value
+        filtered_values[step] = previous_ma_value
 
     return filtered_values
     
 
-# data = np.array([5.3, 6.7, 7.9, 7.1, 5.2, 4.1, 3.5, 5.4, 7.3, 9.4, 8, 6.6, 7.9, 9.2, 7.6])
-# n = 4
 
-# new_data = exponential_moving_avereage(data, n)
-
-# plt.plot(data)
-# plt.plot(new_data)
-# plt.show()
 
--- moving_average.py
+++ moving_average.py
@@ -2,8 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def moving_average(data, smooth_interval=2):
+
+    if(smooth_interval > len(data)):
+        print("Smooth interval > lenght of data")
+        return
+    
     sum = 0
     new_data = np.zeros(len(data))
+
+    for i in range (smooth_interval):
+        for j in range(smooth_interval):
+            sum += data[i+j]
+        average = sum/smooth_interval
+        new_data[i] = average
+        sum = 0
+
     for i in range(len(data)-smooth_interval):
         for j in range(smooth_interval):
             sum += data[i+j]
@@ -12,12 +25,3 @@ def moving_average(data, smooth_interval=2):
         sum = 0
     return new_data
 
-# data = np.array([5.3, 6.7, 7.9, 7.1, 5.2, 4.1, 3.5, 5.4, 7.3, 9.4, 8, 6.6, 7.9, 9.2, 7.6])
-# x = [0 for i in range(len(data))]
-# n = 3
-
-# new_data = moving_average(data, n)
-
-# plt.plot(data)
-# plt.plot(new_data)
-# plt.show()
